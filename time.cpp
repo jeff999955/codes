0a70e491d64d0ada06df01e9d90f7b5c147c9c9f
jeff999955 <jeff999955@gmail.com>
2017-10-24 21:03:05 +0800

    fc

---
+++ time.cpp
@@ -0,0 +1,22 @@
+#include <ctime> //clock()
+#include <vector>
+#include <algorithm> // for random_shuffle
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+	clock_t start_time = clock();
+	if((clock() - start_time) / (CLOCKS_PER_SEC) > 7)
+	{
+		printf("%d\n", 1234);
+		return 0;
+	}
+	int n = 20;
+	vector<int> vs;
+	for(int i=0;i<n;i++);
+		vs.push_back(i);
+	random_shuffle(vs.begin(),vs.end()); //¥´¶Ãvector 
+	return 0;
+ } 
